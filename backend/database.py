46dfacd2976d60f0790a37f87aba192350c7f79c
Srujan <srujanuppula777@gmail.com>
2026-03-27 10:22:28 +0530

    test hardcoded db url

--- backend/database.py
+++ backend/database.py
@@ -3,20 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL environment variable is not set!")
-
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+# Hardcoded temporarily to test
+DATABASE_URL = "postgresql://postgres:Srujan%40331@db.kwpyjilnbroggeryjdpp.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
